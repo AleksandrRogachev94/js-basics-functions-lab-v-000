cde1972955978c864390a8f9ab06595d5b604e0b
Aleksandr Rogachev <aleksandr.rogachev1994@gmail.com>
2017-11-03 01:33:20 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ function calculatesFarePrice(start, destination) {
   if (dist < 400){
     return 0
   } else if(dist > 400 && dist < 2000 ){
-    return dist * 0,02;
+    return dist * 0.02;
   } else if (dist > 2000){
     return 25;
   } else{
