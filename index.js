51937e38055092acb2806800d056fcab89f2f96d
Aleksandr Rogachev <aleksandr.rogachev1994@gmail.com>
2017-11-03 00:55:49 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,12 +1,11 @@
 // Code your solution in this file!
-let blocks;
 
 function distanceFromHqInBlocks(currentStreet){
   blocks = Math.abs(42 - currentStreet);
   return blocks;
 }
 
-function distanceFromHqInFeet(){
-  distance = blocks * 264;
+function distanceFromHqInFeet(blocks){
+  distance = distanceFromHqInBlocks() * 264;
   return distance;
 }
