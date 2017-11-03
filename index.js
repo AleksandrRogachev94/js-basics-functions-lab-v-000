4a2287680f074b2c2afba4b1312972a1a6189f88
Aleksandr Rogachev <aleksandr.rogachev1994@gmail.com>
2017-11-03 01:31:49 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -13,3 +13,16 @@ function distanceFromHqInFeet(currentStreet){
 function distanceTravelledInFeet (start, destination){
   return Math.abs(destination - start) * 264;
 }
+
+function calculatesFarePrice(start, destination) {
+  let dist = distanceTravelledInFeet(start, destination);
+  if (dist < 400){
+    return 0
+  } else if(dist > 400 && dist < 2000 ){
+    return dist * 0,02;
+  } else if (dist > 2000){
+    return 25;
+  } else{
+    return ("cannot travel that far");
+  }
+}
